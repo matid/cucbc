e4abde7ed75049ef67829347d922dd3a1878a504
Mateusz Drożdżyński <matid@matid.net>
2010-10-23 17:13:20 +0100

    Use OAuth for Twitter

--- notifiers/twitter.rb
+++ notifiers/twitter.rb
@@ -1,16 +1,15 @@
-require "twitter"
-
 module CUCBC
   module Notifier
     class Twitter < Base
       def self.connect
         config = YAML::load(File.open("twitter.yml"))
-        
-        auth = ::Twitter::HTTPAuth.new(config["username"], config["password"])
-        
-        @client = ::Twitter::Base.new(auth)
+
+        oauth = ::Twitter::OAuth.new(config["consumer_token"], config["consumer_secret"])
+        oauth.authorize_from_access(config["oauth_token"], config["oauth_token_secret"])
+
+        @client = ::Twitter::Base.new(oauth)
       end
-      
+
       def self.update(message)
         connect
         @client.update(message)
