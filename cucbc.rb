062cca284bad9572ce9fe102985a80a0dc55f671
Mateusz Drożdżyński <matid@matid.net>
2010-10-23 17:13:04 +0100

    Use Bundler and RVM

--- cucbc.rb
+++ cucbc.rb
@@ -1,11 +1,12 @@
-require "flag"
-require "notifier"
-require "notifiers/twitter"
+require "rubygems"
+require "bundler/setup"
+
+["flag", "notifier", "notifiers/screen", "notifiers/twitter"].each { |p| require File.expand_path(p) }
 
 module CUCBC
   class Base
     attr_accessor :notifiers, :flag
-    
+
     def initialize
       notifiers = [Notifier::Twitter]
       if Flag.changed?
